f49ebdfa8d96da1d7f3a59908eb5a39d8239bc46
Kavindu Rashmika <rkavidu222@gmail.com>
2024-06-25 12:25:14 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'firstProject';
+  heading = 'Welcome To Angular';
+  pharagraph = 'First Angular code'
 }
